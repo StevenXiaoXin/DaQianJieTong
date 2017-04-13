8fe37fe082dd42c6e6dce2b94e9b960d0c95d668
StevenXiaoXin <Steven_liu0722@163.com>
2017-04-13 17:11:56 +0800

    工具类

---
+++ app/src/main/java/daqianjietong/com/utils/HttpUtil.java
@@ -0,0 +1,66 @@
+package daqianjietong.com.utils;
+
+import android.widget.Toast;
+
+import org.xutils.common.Callback;
+import org.xutils.ex.HttpException;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+/**
+ * Created by Administrator on 2017/4/13 0013.
+ * xUtils的网络请求工具类
+ */
+
+public class HttpUtil {
+
+    public  String getRequest(String url){
+        RequestParams params = new RequestParams(url);
+
+        x.http().get(params, new Callback.CommonCallback<String>() {
+
+
+
+            String result;
+            @Override
+            public void onSuccess(String result) {
+                this.result=result;
+                Toast.makeText(x.app(), result, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Toast.makeText(x.app(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                if (ex instanceof HttpException) { // 网络错误
+                    HttpException httpEx = (HttpException) ex;
+                    int responseCode = httpEx.getCode();
+                    String responseMsg = httpEx.getMessage();
+                    String errorResult = httpEx.getResult();
+                    // ...
+                } else { // 其他错误
+                    // ...
+                }
+            }
+
+
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+            //取消请求，后期可以加取消提示
+            }
+
+
+            // 不管成功或者失败最后都会回调该接口，把最后必须处理的放在这个方法中。
+            @Override
+            public void onFinished() {
+
+            }
+        });
+
+        String result="";
+        return result;
+    }
+    public void setResult(String result){
+
+    }
+}
