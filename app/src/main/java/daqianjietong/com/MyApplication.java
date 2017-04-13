6eb36acc9a68574012ba581ed15f94f045f36406
StevenXiaoXin <Steven_liu0722@163.com>
2017-04-13 17:12:27 +0800

    自定义

--- app/src/main/java/daqianjietong/com/MyApplication.java
+++ app/src/main/java/daqianjietong/com/MyApplication.java
@@ -2,9 +2,17 @@ package daqianjietong.com;
 
 import android.app.Application;
 
+import org.xutils.x;
+
+
 /**
  * Created by Administrator on 2017/4/13 0013.
  */
 
 public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+    }
 }
