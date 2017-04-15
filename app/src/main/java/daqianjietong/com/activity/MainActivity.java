0a436b98da41c98f522556bb2745bd56d8844421
StevenXiaoXin <Steven_liu0722@163.com>
2017-04-15 17:41:38 +0800

    自定义修改

--- app/src/main/java/daqianjietong/com/activity/MainActivity.java
+++ app/src/main/java/daqianjietong/com/activity/MainActivity.java
@@ -1,18 +1,35 @@
 package daqianjietong.com.activity;
 
-
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+import daqianjietong.com.BaseActivity;
+import daqianjietong.com.adapter.SectionPagerAdapter;
 import daqianjietong.com.daqianjietong.R;
-import daqianjietong.com.utils.HttpUtil;
 
-public class MainActivity extends Activity {
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+    @ViewInject(R.id.container)
+    private ViewPager mViewPager;
+
+    @ViewInject(R.id.toolbar)
+    private Toolbar toolbar;
+
+    @ViewInject(R.id.tabs)
+    private TabLayout tabLayout;
+
+    private SectionPagerAdapter mSectionPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
     }
 }
