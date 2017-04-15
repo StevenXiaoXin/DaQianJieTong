9525082b660c9dc642f089985bf15e625df3d783
StevenXiaoXin <Steven_liu0722@163.com>
2017-04-15 17:41:22 +0800

    自定义修改

---
+++ app/src/main/java/daqianjietong/com/adapter/SectionPagerAdapter.java
@@ -0,0 +1,38 @@
+package daqianjietong.com.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Administrator on 2017/4/15 0015.
+ */
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+    public SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "第1页";
+            case 1:
+                return "第2页";
+            case 2:
+                return "第3页";
+        }
+        return null;
+    }
+}
