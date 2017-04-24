09e1a8f59469e7aa31f64df07e74ba2ee4362608
StevenXiaoXin <Steven_liu0722@163.com>
2017-04-24 10:42:06 +0800

    自定义修改

--- app/src/main/java/daqianjietong/com/activity/UserLoginActivity.java
+++ app/src/main/java/daqianjietong/com/activity/UserLoginActivity.java
@@ -3,7 +3,10 @@ package daqianjietong.com.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -21,6 +24,23 @@ public class UserLoginActivity extends BaseActivity{
 
     @ViewInject(R.id.user_login_image)
     private ImageView user_login_image;
+
+    @ViewInject(R.id.user_name)
+    private EditText user_name;
+
+    @ViewInject(R.id.user_password)
+    private EditText user_password;
+
+    @ViewInject(R.id.btn_login)
+    private Button btn_login;
+
+    @ViewInject(R.id.tv_new_user)
+    private TextView tv_new_user;
+
+    @ViewInject(R.id.tv_forget_psd)
+    private TextView tv_froget_psd;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
